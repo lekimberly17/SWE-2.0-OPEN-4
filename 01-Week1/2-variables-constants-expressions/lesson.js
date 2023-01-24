f5ecde34fbece12d72d24d5e3f73bdba1de8f9ee
Kim <lekimberly509@gmail.com>
2023-01-24 10:02:12 -0800

    changes to homework week 1

---
+++ 01-Week1/2-variables-constants-expressions/lesson.js
@@ -0,0 +1,11 @@
+// let variable
+let age = 25
+age = 30
+age = false
+
+// const variable
+const port = '3000'
+
+// Expressions
+let combineString = "hello" + "world" + "!"
+console.log(combineString)
