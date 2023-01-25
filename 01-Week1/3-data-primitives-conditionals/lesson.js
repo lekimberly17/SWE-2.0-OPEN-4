223e99f3d9c39ca50c795b6d98204eea5f99a759
lekimberly17 <lekimberly509@gmail.com>
2023-01-25 09:45:21 -0800

    update for day 3 notes

---
+++ 01-Week1/3-data-primitives-conditionals/lesson.js
@@ -0,0 +1,92 @@
+//COPY ME TO YOUR PERSONAL FOLDER - DO NOT EDIT HERE
+//Warm up exercise
+/*
+
+Determine whether or not the following is appropriately labeled? 
+    let a = '4' // not specific
+    // temperature
+
+    const height = 35 // height is dynamic we should change it to a let variable
+
+Data Primitives
+
+    Symbol
+    Number - Integers, decimals
+    String - anything in double or single quotes
+    boolean - True or False
+
+    null - a variable that has data stored in it and the value is null
+    undefined - a variable that has no data stored in it
+
+        Example
+
+            let data
+            console.log("Data", data)
+
+            let someInfo = null
+            console.log("SomeInfo", SomeInfo)
+
+Control Flow - conditional statement
+        let budget = 1000
+        const hotelRoomCost = 375
+
+        // if my budget is more than the hotelroomcost -> go on trip
+        // if not, I am too broke to travel
+
+        if(budget > hotelRoomCost) {
+            // this will execute of the condidition is met
+            console.log('LETS PACK OUR BAGS AND GOOOO!')
+        } else {
+            // this condition will execute of the condition is false
+            console.log('I'm too broke to go....')
+        }
+
+
+
+Conditional statement - if / else if / else
+
+        let grade = "B"
+        
+        if(grade === "A") {
+            // if this condition is met
+            console.log("Excellent work!")
+        } else if(grade === "C") {
+            // if the orignal condition is not met but the else if condition is met
+            console.log("Can improve")
+        } else {
+            // if all conditions are not met
+            console.log("Come to office hours!!")
+        }
+
+
+Comparison Operators
+    let height = 100
+
+    (height >= 40)
+
+    let budget = 3000
+
+    (budget > 2000)
+
+    const passport = true
+
+    passport === true)
+
+    console.log('equal to', 3 == '3') // true == 
+    only compares the value not the data type
+        // js will corece any numbers into a string and pare the two strings '3' == '3'
+    console.log('strictly equals to', 3 === '3') // false === c
+    ompares both value and data type 3 === '3'
+
+
+Logical Operators
+    let budget = 3000
+    let passport = true
+
+    (budget >= 4000 && passport === true) // How is this conditional
+    statement evaluated
+
+
+Pair Programming exercise
+
+*/
